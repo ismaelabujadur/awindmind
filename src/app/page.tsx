f26ac5f43d61847b2cda2f56c170de51840b1844
ismaelabujadur <ismaelabujadur@gmail.com>
2025-10-07 12:27:52 +0200

    Add img and animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="">
-      <main className="flex justify-center items-center w-full h-full">
+      <main className="flex justify-center items-center w-full h-full bg-black">
         <Image
-          className="dark:invert"
-          src="/temp-logo.png"
+          className="dark:invert w-screen h-screen object-contain object-center animate-[spin_300s_infinite] lg:p-12"
+          src="/Troll.png"
           alt="Wind logo"
           width={800}
           height={800}
